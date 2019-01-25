c7f5e5fd6c594af9190729df9ec12ff588b681ef
Andrew White <andrew.white@unboxed.co>
2019-01-25 06:33:08 +0000

    Remove deprecated config in Rails 6.0
    As of rails/rails@f59b081 booleans are always represented as integers
    using the Active Record SQLite3Adapter.

--- example_app_generator/config/initializers/sqlite3_fix.rb
+++ example_app_generator/config/initializers/sqlite3_fix.rb
@@ -1,3 +1,5 @@
 if Rails.application.config.respond_to?(:active_record)
-  Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+  if Rails.version.to_f < 6.0
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+  end
 end
